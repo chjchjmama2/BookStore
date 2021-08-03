9d94be788c6fd2956a279b02df04f51234088f77
Quang Trường <votruong284@gmail.com>
2021-08-03 20:54:39 +0700

    final

--- dangkyvadangnhap.php
+++ dangkyvadangnhap.php
@@ -12,26 +12,6 @@
 </head>
 
 <body>
-	<!-- Open Header -->
-	<!-- <div class="menu">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-              <a class="navbar-brand" href="#"><img src="image/logo.png" alt="..." width="100px"></a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">Trang chủ</a>
-                  </li>                      
-                </ul>
-              </div>
-            </div>
-          </nav>
-    </div> -->
-	<!-- End Header -->
-	<!-- Dang ky va dang nhap -->
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6 col-md-offset-3">
